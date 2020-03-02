356ffd4d31354e9c3c5f866c1fb9d81fd6d6ca2e
Hani Bounoua <bounoua.hani@gmail.com>
2020-03-02 19:17:57 +0100

    final

---
+++ Newton_s_Algorithms.py
@@ -0,0 +1,42 @@
+
+
+
+def Newton_root_Algorithm(x_0, fun, dx, threshold):
+    y = []
+    dydx = []
+    x = []
+    x.append(x_0)
+    y.append(fun(x[0]))
+    dydx.append(dx(x[0]))
+    i = 1
+    while True:
+        x.append(x[i-1] - y[i-1]/dydx[i-1])
+        y.append(fun(x[i]))
+        dydx.append(dx(x[i]))
+        if abs(y[i]) <= threshold:
+            break
+        else:
+            i += 1
+    return {"x": x, "Function Values": y, "First derrivative repect to x values": dydx}
+
+
+def Newton_optimization_algrithm(x_0, fun, dfdx, ddfddx, learningRate, threshold):
+    y = []
+    dydx = []
+    ddyddx = []
+    x = []
+    x.append(x_0)
+    y.append(fun(x[0]))
+    dydx.append(dfdx(x[0]))
+    ddyddx.append(ddfddx(x[0]))
+    i = 1
+    while True:
+        x.append(x[i-1] - learningRate * dydx[i-1]/ddyddx[i-1])
+        y.append(fun(x[i]))
+        dydx.append(dfdx(x[i]))
+        ddyddx.append(ddfddx(x[i]))
+        if abs(dydx[i]) <= threshold:
+            break
+        else:
+            i += 1
+    return {"x": x, "Function Values": y, "Tangent Slope": dydx}
--- demo.py
+++ demo.py
@@ -1,7 +1,10 @@
-from Root_finding_methods import Newton_root_methode
+from Newton_s_Algorithms import Newton_root_Algorithm, Newton_optimization_algrithm
 
 def f(x):
     return  x**2 + x - 2
 def df(x):
     return 2*x + 1
-print(Newton_root_methode(0, f, df, .001))
+def ddf(x):
+    return 2
+print(Newton_root_Algorithm(0, f, df, .001))
+print(Newton_optimization_algrithm(0, f, df, ddf, .1, .0001))
